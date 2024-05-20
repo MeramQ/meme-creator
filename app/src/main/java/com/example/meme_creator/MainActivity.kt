82f41aed3bcfb8430f4a136090b799042b28306b
Marcin Lesiak <marcinui07@gmail.com>
2024-05-20 23:33:38 +0200

    added function to draw text on images

--- app/src/main/java/com/example/meme_creator/MainActivity.kt
+++ app/src/main/java/com/example/meme_creator/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.meme_creator
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 
 class MainActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private lateinit var topEditText: EditText
     private lateinit var bottomEditText: EditText
     private lateinit var generateButton: Button
-    private val PICK_IMAGE = 1
+    private val pickImage = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,21 +31,58 @@ class MainActivity : AppCompatActivity() {
         imageView.setOnClickListener {
             openGallery()
         }
+
+        generateButton.setOnClickListener {
+            val text1 = topEditText.text.toString()
+            val text2 = bottomEditText.text.toString()
+            val bitmap = imageView.drawable.toBitmap()
+            val scaledBitmap = scaleBitMap(bitmap)
+            imageView.setImageBitmap(scaledBitmap)
+            val resultBitmap = drawTextOnBitmap(scaledBitmap, text1, text2)
+            imageView.setImageBitmap(resultBitmap)
+        }
     }
 
     private fun openGallery() {
         val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(galleryIntent, PICK_IMAGE)
+        startActivityForResult(galleryIntent, pickImage)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+        if (requestCode == pickImage && resultCode == RESULT_OK && data != null) {
             val selectedImage = data.data
             val bitmap =
                 BitmapFactory.decodeStream(contentResolver.openInputStream(selectedImage!!))
             imageView.setImageBitmap(bitmap)
         }
+    }
+
+    private fun scaleBitMap(bitmap: Bitmap): Bitmap {
+        val width: Int = bitmap.getWidth()
+        val height: Int = bitmap.getHeight()
+        val newWidth = width / 2
+        val newHeight = height / 2
+        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
+    }
+
+    // TODO: add option to change font by user
+    private fun drawTextOnBitmap(bitmap: Bitmap, text1: String, text2: String): Bitmap {
+        val mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val canvas = Canvas(mutableBitmap)
+        val paint = Paint()
+        paint.textSize = mutableBitmap.height / 7f
+        paint.textAlign = Paint.Align.CENTER
+        paint.color = android.graphics.Color.WHITE
+        paint.isAntiAlias = true
+        canvas.drawText(text1, mutableBitmap.width / 2f, mutableBitmap.height / 8f, paint)
+        canvas.drawText(
+            text2,
+            mutableBitmap.width / 2f,
+            mutableBitmap.height - mutableBitmap.height / 30f,
+            paint
+        )
+        return mutableBitmap
     }
 }
